5c94d4b6ab27d38f818d40f7b177149996f2788b
novirael <novirael@gmail.com>
2016-05-25 22:10:48 +0200

    Rendering form fields

--- static/js/components/questionnaire/QuestionnaireForm.js
+++ static/js/components/questionnaire/QuestionnaireForm.js
@@ -1,12 +1,13 @@
 'use strict';
 
 import React, { Component, PropTypes } from 'react';
-import { Nav, NavItem, Navbar } from 'react-bootstrap';
+import { Form, FormGroup, Col, Button } from 'react-bootstrap';
 
 import GlobalLayout from '../layouts/GlobalLayout';
 import NotFound from '../NotFound';
 
 import QuestionnaireHeader from './QuestionnaireHeader';
+import QuestionnaireField from './QuestionnaireField';
 
 
 export default class QuestionnaireForm extends GlobalLayout {
@@ -29,20 +30,41 @@ export default class QuestionnaireForm extends GlobalLayout {
     }
 
     renderContent() {
+        var form = null;
+
         if (this.state.schemaLoaded && !this.state.schema.url) {
             return <NotFound
                 title={"Questionnaire Not Found!"}
                 body={"Questionnaire cannot be found in current location. Please make sure that you have proper access and questionnaire is still valid."}
                 renderAsLayout={false}
             />;
+        } else if (this.state.schemaLoaded) {
+            form = <Form horizontal>
+                {this.renderFields()}
+                <FormGroup>
+                    <Col smOffset={2} sm={10}>
+                        <Button type="submit">{"Save my answers"}</Button>
+                    </Col>
+                </FormGroup>
+            </Form>;
         }
 
-        return <QuestionnaireHeader
-            title={this.state.schema.title}
-            description={this.state.schema.description}
-        />;
+        return <div>
+            <QuestionnaireHeader
+                title={this.state.schema.title}
+                description={this.state.schema.description}
+            />
+            <div className="container">
+                {form}
+            </div>
+        </div>;
     }
 
+    renderFields() {
+        return this.state.schema.fields.map(function (field) {
+            return <QuestionnaireField field={field} key={field.id} />
+        });
+    }
 };
 
 QuestionnaireForm.propTypes = {
