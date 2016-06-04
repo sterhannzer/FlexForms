4c7868750d2cc29e26ae26f5f18f2d3cede46587
novirael <novirael@gmail.com>
2016-06-04 16:01:12 +0200

    Initial api for questionnaire summary

--- forms/urls.py
+++ forms/urls.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from django.conf.urls import url, include
+from django.contrib.auth.decorators import login_required
 from rest_framework import routers
 
-from forms.views import SchemaViewSet, QuestionnaireViewSet
+from forms.views import SchemaViewSet, QuestionnaireViewSet, QuestionnaireSummaryView
 
 router = routers.DefaultRouter()
 router.register(r'schema', SchemaViewSet)
@@ -11,4 +12,6 @@ router.register(r'questionnaire', QuestionnaireViewSet)
 
 urlpatterns = [
     url(r'^api/', include(router.urls)),
+
+    url(r'^api/v1/summary/(?P<schema_id>\d+)[/]?$', login_required(QuestionnaireSummaryView.as_view()), name='summary_details'),
 ]
--- forms/views.py
+++ forms/views.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from forms.models import Schema, Questionnaire
 
 from forms.serializers import SchemaSerializer, QuestionnaireSerializer
@@ -14,3 +17,25 @@ class SchemaViewSet(viewsets.ReadOnlyModelViewSet):
 class QuestionnaireViewSet(viewsets.ModelViewSet):
     queryset = Questionnaire.objects.all()
     serializer_class = QuestionnaireSerializer
+
+
+class QuestionnaireSummary(object):
+    schema = None
+
+    def __init__(self, schema_id=None):
+        if schema_id:
+            self.schema = Schema.objects.get(id=schema_id)
+
+    def fetch(self):
+        if not self.schema:
+            return
+
+        return []
+
+
+class QuestionnaireSummaryView(APIView):
+
+    def get(self, request, *args, **kwargs):
+        summary = QuestionnaireSummary(schema_id=kwargs.get('schema_id'))
+        result = summary.fetch()
+        return Response(result, status=200)
